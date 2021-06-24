eadfc8c128f6cfcef4cd499dabcc2a1a52a6375e
cutsea110 <cutsea110@gmail.com>
2021-06-25 04:51:02 +0900

    refactor

--- src/Aop01.hs
+++ src/Aop01.hs
@@ -4,16 +4,16 @@ import Calculations (Calculation)
 import Simplify (prove, simplify)
 
 append_assoc_nil :: Calculation
-append_assoc_nil = prove customLaws "append x (append y nil) = append (append x y) nil"
+append_assoc_nil = prove customLaws "x ++ (y ++ nil) = (x ++ y) ++ nil"
   where
-    customLaws = [ "++ の定義(基底): append x nil = x"
-                 , "++ の定義(再帰): append x (snoc y a) = snoc (append x y) a"
+    customLaws = [ "++ の定義(基底): x ++ nil = x"
+                 , "++ の定義(再帰): x ++ (snoc y a) = snoc (x ++ y) a"
                  ]
 
 append_assoc_snoc :: Calculation
-append_assoc_snoc = prove customLaws "append x (append y (snoc z a)) = append (append x y) (snoc z a)"
+append_assoc_snoc = prove customLaws "x ++ (y ++ (snoc z a)) = (x ++ y) ++ (snoc z a)"
   where
-    customLaws = [ "++ の定義(基底): append x nil = x"
-                 , "++ の定義(再帰): append x (snoc y a) = snoc (append x y) a"
-                 , "帰納法の仮定: append x (append y z) = append (append x y) z"
+    customLaws = [ "++ の定義(基底): x ++ nil = x"
+                 , "++ の定義(再帰): x ++ (snoc y a) = snoc (x ++ y) a"
+                 , "帰納法の仮定: x ++ (y ++ z) = (x ++ y) ++ z"
                  ]
