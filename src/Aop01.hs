3b89f50246c995dcde6fed34e25983758b5401b3
cutsea110 <cutsea110@gmail.com>
2021-06-24 10:53:35 +0900

    testing!

--- src/Aop01.hs
+++ src/Aop01.hs
@@ -6,13 +6,14 @@ import Simplify (prove, simplify)
 append_assoc_nil :: Calculation
 append_assoc_nil = prove customLaws "append x (append y nil) = append (append x y) nil"
   where
-    customLaws = [ "++ の最初の定義式: append x nil = x"
-                 , "++ の2番目の定義式: append x (snoc y a) = snoc (append x y) a"
+    customLaws = [ "++ の定義(基底): append x nil = x"
+                 , "++ の定義(再帰): append x (snoc y a) = snoc (append x y) a"
                  ]
 
-append_assoc_cons :: Calculation
-append_assoc_cons = prove customLaws "append x (append y (snoc z a)) = append (append x y) (snoc z a)"
+append_assoc_snoc :: Calculation
+append_assoc_snoc = prove customLaws "append x (append y (snoc z a)) = append (append x y) (snoc z a)"
   where
-    customLaws = [ "++ の最初の定義式: append x nil = x"
-                 , "++ の2番目の定義式: append x (snoc y a) = snoc (append x y) a"
+    customLaws = [ "++ の定義(基底): append x nil = x"
+                 , "++ の定義(再帰): append x (snoc y a) = snoc (append x y) a"
+                 , "帰納法の仮定: append x (append y z) = append (append x y) z"
                  ]
