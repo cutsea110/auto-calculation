266587f2e22a85c2285864275b8a004c99326127
cutsea110 <cutsea110@gmail.com>
2021-06-26 04:55:09 +0900

    couldn't parse (f,g)

--- src/Aop01.hs
+++ src/Aop01.hs
@@ -17,3 +17,22 @@ append_assoc_snoc = prove customLaws "x ++ (y ++ (snoc z a)) = (x ++ y) ++ (snoc
                  , "++ の定義(再帰): x ++ (snoc y a) = snoc (x ++ y) a"
                  , "帰納法の仮定: x ++ (y ++ z) = (x ++ y) ++ z"
                  ]
+
+test :: Calculation
+test = prove customLaws "listr outl . filter outr . zip . pair (id, listr p) = filter p"
+
+customLaws = [ "filterの定義: filter p = concat . listr (p -> wrap , nilp)"
+             , "等式(1.1): outl . pair (f , g) = f"
+             , "等式(1.2): outr . pair (f , g) = g"
+             , "等式(1.3): nilp . f = nilp"
+             , "等式(1.4): listr f . nilp = nilp"
+             , "等式(1.5): listr f . wrap = wrap . f"
+             , "等式(1.6): listr f . concat = concat . listr (listr f)"
+             , "等式(1.7): zip . pair (listr f , listr g) = listr (pair (f , g))"
+             , "等式(1.8): listr (f , g) = listr f . listr g"
+             , "等式(1.9): listr id = id"
+             , "等式(1.10): (p -> f , g) . h = (p . h -> f . h , g . h)"
+             , "等式(1.11): h . (p -> f , g) = (p -> h . f , h . g)"
+             , "等式(1.12): f . id = f"
+             , "等式(1.13): id . f = id"
+             ]
